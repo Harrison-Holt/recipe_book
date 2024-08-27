49d3fda8ed08a64f9213d27bd7c0359e5b2db2de
Harrison-Holt <hholt2901@gmail.com>
2024-08-27 09:39:06 -0400

    first commit 2.2

---
+++ public/register.js
@@ -0,0 +1,29 @@
+
+document.getElementById("register_submit_button").addEventListener('click', async function(event) {
+    event.preventDefault(); 
+
+    let username = document.getElementById("username").value; 
+    let password = document.getElementById("password").value; 
+    let email = document.getElementById("email").value; 
+
+
+    try {
+        const response = await fetch('/api/user_post', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify({username, password, email})
+        }); 
+
+        if (response.ok) {
+           window.location.href = './index.html'; 
+        } else {
+            const error = await response.json(); 
+            console.error("Error: ", error); 
+            
+        }
+    } catch(error) {
+        console.error("Error sending user data: ", error); 
+    }
+}); 
