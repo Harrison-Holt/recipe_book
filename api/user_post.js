c7d6f4fa3eb125586b5125830d7f67b936bda5f6
Harrison-Holt <hholt2901@gmail.com>
2024-08-26 20:54:46 -0400

    first commitn 2.0

---
+++ api/user_post.js
@@ -0,0 +1,33 @@
+import connect_database from '../config/db.js'; 
+import bcrypt from 'bcryptjs'; 
+
+export default async function handler(req, res) {
+
+    if(req.method !== 'POST') {
+        res.status(405).json({ message: 'Only POST request are allowed'}); 
+        return; 
+    }
+
+    const { name, password, email } = req.body; 
+
+    if(!name || !password || !email) {
+        res.status(400).json({ message: 'Name, password, email are required!'});
+        return;  
+    }
+
+    try {
+        const hashed_password = await bcrypt.hash(password, 10); 
+
+        const db = await connect_database(); 
+
+        const [result] = await db.execute(
+            'INSERT INTO users (name, password, email) VALUES (?, ?, ?)', 
+            [name, hashed_password, email]
+        ); 
+
+        res.status[201].json({ message: 'User created successfully', userId: result.insertId}); 
+    } catch(error) {
+        console.error('Error insertinmg user data: ', error); 
+        res.status[500].json({ message: 'Internal Server Error'}); 
+    }
+}
