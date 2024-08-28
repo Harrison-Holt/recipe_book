3bc01c642b58f007aeae4e8d1569dbc451afbea1
Harrison-Holt <hholt2901@gmail.com>
2024-08-28 15:37:28 -0400

    first commit 3.9

--- public/js/verify_token.js
+++ public/js/verify_token.js
@@ -1,39 +1,37 @@
-window.onload = function() {
-    const token = localStorage.getItem('token'); 
+function verifyToken() {
+    const token = localStorage.getItem('token');
 
     if (!token) {
-        window.location.href = './login.html'; 
-    } else {
-
-        fetch('/api/verify_token', {
-            method: 'POST', 
-            headers: 
-            {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        })
-
-        .then(response => response.json())
-        .then(data => {
-
-            if(!data.valid) {
-                localStorage.removeItem('token'); 
-                window.location.href = './login.html'; 
-            }
-            else {
-                document.getElementById("user_greeting").textContent = `Welcome, ${data.decoded.username}`; 
-            }
-        })
-        .catch(error => {
-            console.error("Error verifying token: ", error); 
-            localStorage.removeItem('token'); 
-            window.location.href = './login.html'; 
-        }); 
+        window.location.href = './login.html';
+        return;
     }
-}; 
+
+    fetch('/api/verify_token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (!data.valid) {
+            localStorage.removeItem('token');
+            window.location.href = './login.html';
+        } else {
+            document.getElementById("user_greeting").textContent = `Welcome, ${data.decoded.username}`;
+        }
+    })
+    .catch(error => {
+        console.error("Error verifying token: ", error);
+        localStorage.removeItem('token');
+        window.location.href = './login.html';
+    });
+}
+
+window.onload = verifyToken;
 
 document.getElementById("logout").addEventListener('click', function() {
-    localStorage.removeItem('token'); 
-    window.location.href = './login.html'; 
-}); 
+    localStorage.removeItem('token');
+    window.location.href = './login.html';
+});
