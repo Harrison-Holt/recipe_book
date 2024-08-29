5a8fc9e9b19d2579eaad466624b0edc922c8aeeb
Harrison-Holt <hholt2901@gmail.com>
2024-08-29 08:33:11 -0400

    first commit 4.8

--- api/delete_account.js
+++ api/delete_account.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
         const db = await connectToDatabase();
 
-        const [results] = await db.query('DELETE FROM users WHERE id = ?', [userId]);
+        const [results] = await db.query('DELETE FROM user WHERE id = ?', [id]);
 
         await db.end(); 
 
