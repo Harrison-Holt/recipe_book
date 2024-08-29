f70d663ba9c17fe39e8366eed9beefe28fb5972c
Harrison-Holt <hholt2901@gmail.com>
2024-08-29 08:22:48 -0400

    first commit 4.7

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,8 @@ document.getElementById("settings").addEventListener('click', function() {
 
     window.location.href = './settings.html'; 
 }); 
+
+document.getElementById("logout").addEventListener('click', function() {
+
+    window.location.href = './login.html'; 
+}); 
